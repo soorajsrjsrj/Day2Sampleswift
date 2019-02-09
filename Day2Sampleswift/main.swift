f7fec3710c7c89c04ed6cdb2d0aab1e225828103
MacStudent <macstudent@126-iMac30.local>
2019-02-08 21:03:01 -0500

    Dictionary

--- Day2Sampleswift/main.swift
+++ Day2Sampleswift/main.swift
@@ -114,7 +114,142 @@ print(t[1])
 // Declaration using array<Type>()
 var m = Array<Int>()
 m.append(500)
-print(m(0))
+print(m[0])
+
+
+// input a = [1, 2, 3, 4, 5]
+//output x = [120, 60, 40, 30, 24]
+let x0 = [1, 2, 3, 4, 5]
+var y0 = Array(repeating: 0, count: x0.count)
+
+for i in 0..<x0.count
+{
+    var t = 1
+    for j in 0..<x0.count
+    {
+        if(j != i)
+        {
+            t *= x0[j]
+        }
+    }
+    y0[i] = t
+}
+
+print(x0)
+print(y0)
+
+
+//
+
+
+print("--set example--")
+var s1 = Set<String>()
+s1.insert("canada")
+s1.insert("usa")
+s1.insert("india")
+s1.insert("russia")
+s1.insert("brazil")
+s1.insert("china")
+
+print(s1)
+
+//will not insert
+s1.insert("india")
+print(s1)
+
+//will insert duplication will not effect case sensitive
+s1.insert("India")
+print(s1)
+
+//Dictionary example
+var d = [1:"abc",
+         2:"hello"]
+print("-----Dictionary example")
+print(d)
+
+d[3] = "hello world"
+print(d)
+
+
+print(d[4])
+
+//
+var country = Dictionary<String, String>()
+country.updateValue("india", forKey: "ind")
+country.updateValue("china", forKey: "chi")
+country.updateValue("brazil", forKey: "bzl")
+country.updateValue("maxico", forKey: "mxo")
+
+
+print(country)
+print("-----------------------------------")
+for i in country
+{
+    print(i)
+    print(i.key,i.value)
+}
+print("-----------------------------------")
+for (k,v) in country
+{
+   print(k,v)
+}
+print("-----------------------------------")
+for (_,v) in country
+{
+    print( v)
+}
+print("-----------------------------------")
+for k in country
+{
+    print(k)
+}
+print("-----------------------------------")
+for v in country
+{
+print(v)
+}
+print("-----------------------------------")
+
+//delete all elements /clear
+
+d = [:]                      //
+print(d)
+d.removeAll()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
